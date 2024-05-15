6410548f0694cfc8914f2cc1464ca63a6a274488
SavinaK03 <158211451+SavinaK03@users.noreply.github.com>
2024-05-15 17:54:45 +0300

    1.Reverse a String

---
+++ 1. Reverse a String/1. Reverse a String/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Reverse_a_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+            Stack<char> reversed= new Stack<char>();
+            for (int i = 0; i < line.Length; i++)
+            {
+                reversed.Push(line[i]);
+            }
+            foreach (var ch in reversed)
+            {
+                Console.Write(ch);
+            }
+        }
+    }
+}
